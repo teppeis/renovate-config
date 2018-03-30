61323b7ff5e702016c2ce23af5edd8d678b51a91
teppeis <teppeis@gmail.com>
2018-03-30 12:39:18 +0900

    test: use mocha and power-assert

--- test/validate-config.js
+++ test/validate-config.js
@@ -6,17 +6,34 @@ const {validateConfig} = require('renovate/lib/config/validation');
 
 initLogger();
 
-const pkg = require('../package.json');
-const config = pkg['renovate-config'];
-assert(config, 'package.json > renovate-config is not found');
+describe('@teppeis/renovate-config', () => {
+  let pkg, renovateConfig;
+  beforeEach(() => {
+    pkg = require('../package.json');
+    renovateConfig = pkg['renovate-config'];
+  });
+  afterEach(() => {
+    pkg = null;
+    renovateConfig = null;
+  });
 
-assertConfig(config, 'default');
-assertConfig(config, 'anytime');
+  it('package.json has "renovate-config"', () => {
+    assert(renovateConfig);
+  });
 
-async function assertConfig(renovateConfig, name) {
-  const config = renovateConfig[name];
-  assert(config, `package.json > renovate-config > ${name} is not found`);
-  const {errors, warnings} = await validateConfig(config);
-  assert.deepEqual(errors, []);
-  assert.deepEqual(warnings, []);
-}
+  assertConfig('default');
+  assertConfig('anytime');
+
+  function assertConfig(name) {
+    it(`"renovate-config" has "${name}"`, async () => {
+      assert(renovateConfig[name]);
+    });
+
+    it(`"${name}" is valid`, async () => {
+      const config = renovateConfig[name];
+      const {errors, warnings} = await validateConfig(config);
+      assert.deepEqual(errors, []);
+      assert.deepEqual(warnings, []);
+    });
+  }
+});
