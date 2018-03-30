ead52d14fadd89acb35fe0490cf73d8e09b56275
teppeis <teppeis@gmail.com>
2018-03-30 12:24:17 +0900

    test: since renovate@11.42, validateConfig() returns a Promise

--- test/validate-config.js
+++ test/validate-config.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const assert = require('assert');
+const {initLogger} = require('renovate/lib/logger');
 const {validateConfig} = require('renovate/lib/config/validation');
+
+initLogger();
 
 const pkg = require('../package.json');
 const config = pkg['renovate-config'];
@@ -10,10 +13,10 @@ assert(config, 'package.json > renovate-config is not found');
 assertConfig(config, 'default');
 assertConfig(config, 'anytime');
 
-function assertConfig(renovateConfig, name) {
+async function assertConfig(renovateConfig, name) {
   const config = renovateConfig[name];
   assert(config, `package.json > renovate-config > ${name} is not found`);
-  const {errors, warnings} = validateConfig(config);
+  const {errors, warnings} = await validateConfig(config);
   assert.deepEqual(errors, []);
   assert.deepEqual(warnings, []);
 }
